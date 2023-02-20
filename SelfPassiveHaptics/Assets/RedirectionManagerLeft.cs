b5164193fe92b88ceaa8c30b37f026c2e059a75c
mkogw <mkogw2016@icloud.com>
2023-02-20 17:46:45 +0900

    develop self passive haptics

---
+++ SelfPassiveHaptics/Assets/RedirectionManagerLeft.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedirectionManagerLeft : MonoBehaviour
+{
+    /// <summary>
+    /// </summary>
+    public GameObject realHand;
+    /// <summary>
+    /// The game object of the virtual hand
+    /// </summary>
+    public GameObject virtualHand;
+    /// <summary>
+    /// The warp origin that is used by all redirection techniques. If it is set to NONE, it will
+    /// be set to the hand's real position on the start of each redirection
+    /// </summary>
+    public GameObject warpOrigin;
+    /// <summary>
+
+    /// <summary>
+    /// The Redirection Technique in the Redirection Manager serves as the default Redirection
+    /// Technique, which is used by all Redirected Prefabs, that do not specify another technique 
+    /// </summary>
+    public GameObject redirectionTechnique;
+    /// <summary>
+
+    private HR_Toolkit.Zenner_BodyWarping Zenner_BodyWarping;
+    private HR_Toolkit.RedirectionObject dummyObject;
+    private Transform dummyTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Zenner_BodyWarping = redirectionTechnique.GetComponent<HR_Toolkit.Zenner_BodyWarping>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Zenner_BodyWarping.ApplyRedirection(realHand.transform,virtualHand.transform,warpOrigin.transform,dummyObject,dummyTransform);
+    }
+}
---
+++ SelfPassiveHaptics/Assets/scripts/TaskManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TaskManager : MonoBehaviour
+{
+    public GameObject redirectionTechnique_rightHand;
+    private HR_Toolkit.Zenner_BodyWarping Zenner_BodyWarping;
+
+    public GameObject collisionTarget;
+    private collisionDetector collisionDetector;
+
+    //public float alpha;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Zenner_BodyWarping = redirectionTechnique_rightHand.GetComponent<HR_Toolkit.Zenner_BodyWarping>();
+        collisionDetector = collisionTarget.GetComponent<collisionDetector>();
+
+        //Zenner_BodyWarping.redirectionAngleAlpha = alpha;
+        
+        Debug.Log("Start!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("n"))
+        {
+            Zenner_BodyWarping.redirectionAngleAlpha = Random.Range(-1, 1) * 30.0f ;
+            Debug.Log("current alpha = " + (Zenner_BodyWarping.redirectionAngleAlpha));
+        }
+
+        if(collisionDetector.CollisionDetected){
+            Invoke("GotoNextScene", 1f);
+        }
+    }
+
+    void GotoNextScene()
+    {
+        //SceneManager.LoadScene(ExperimentData.EvaluationScene);
+    }
+}
